5781e4237edb83d4bb539624b38dea540a5cb768
Sergio Alejandro Gonzalez Sánchez <82124793+sergio3144@users.noreply.github.com>
2026-03-30 15:36:06 -0500

    permissions settings

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,10 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
+import { Stack, router } from 'expo-router';
 import 'react-native-reanimated';
+import { useEffect, useRef } from 'react';
+import { AppState } from 'react-native';
+import { usePermissionStore } from '@/store/usePermissions';
+import { PermissionStatus } from '@/infraestructure/interface/location';
 
 import "../global.css";
 
@@ -8,6 +12,31 @@ import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const { checkLocalPermissionState, locationStatus } = usePermissionStore();
+  const appState = useRef(AppState.currentState);
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', nextAppState => {
+      if (
+        appState.current.match(/inactive|background/) &&
+        nextAppState === 'active'
+      ) {
+        checkLocalPermissionState();
+      }
+      appState.current = nextAppState;
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
+  useEffect(() => {
+    // Si nos quitan el permiso mientras la app está corriendo y volvemos (locationStatus cambia)
+    if (locationStatus !== PermissionStatus.GRANTED && locationStatus !== PermissionStatus.CHECKING) {
+      router.replace('/permissions');
+    }
+  }, [locationStatus]);
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
